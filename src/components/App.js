c43737a633ae97291b530a882d7656629b09da8f
Gian Marco Romani <giaro710@gmail.com>
2020-11-03 14:58:24 +0100

    BrowserRouter installed and tried with some routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
+  const componentOne = () => {
+    return <div>This is the main page</div>;
+  };
+
+  const componentTwo = () => {
+    return <div>This is the about page</div>;
+  };
+
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <div>
+        <Route path='/' exact component={componentOne} />
+        <Route path='/about' exact component={componentTwo} />
+      </div>
+    </BrowserRouter>
   )
 }
 
